d5ec7a3ad9fc9664ad0a5d4e1075ee1e8f263f52
Erik Hvatum <ice.rikh@gmail.com>
2014-03-23 20:13:55 -0500

    Replace remaining QtGui::QOpenGL* usage with PyOpenGL calls. Use OpenGL.GL.shaders functions.

--- panel.glslv
+++ panel.glslv
@@ -1,4 +1,4 @@
-#version 410 core
+#version 430 core
 
 uniform mat4 projectionModelViewMatrix;
 
--- ris_widget.py
+++ ris_widget.py
@@ -3,7 +3,7 @@ import numpy
 from OpenGL import GL
 from OpenGL.arrays import vbo
 import OpenGL.GL.ARB.texture_rg
-import OpenGL.GL.shaders
+import OpenGL.GL.shaders as GLS
 import os
 from PyQt5 import QtCore, QtGui, QtWidgets, QtOpenGL
 import sip
@@ -13,18 +13,18 @@ from ris_widget_exceptions import *
 class RisWidget(QtOpenGL.QGLWidget):
     '''RisWidget stands for Rapid Image Stream Widget.  If tearing is visible, try enabling vsync in your OS's display
     settings.  If that doesn't help, supply True for the enableSwapInterval1_ argument.'''
-    def __init__(self, parent_ = None, enableSwapInterval1_ = False):
+    def __init__(self, parent_ = None, windowTitle_ = 'RisWidget', enableSwapInterval1_ = False):
         super().__init__(RisWidget._makeGlFormat(enableSwapInterval1_), parent_)
         self.enableSwapInterval1 = enableSwapInterval1_
         self.imTex = None
-
         self.prevWindowSize = None
+        self.setWindowTitle(windowTitle_)
 
     @staticmethod
     def _makeGlFormat(enableSwapInterval1_):
         glFormat = QtOpenGL.QGLFormat()
         # Our weakest target platform is Macmini6,1 having Intel HD 4000 graphics supporting up to OpenGL 4.1 on OS X
-        glFormat.setVersion(4, 1)
+        glFormat.setVersion(4, 3)
         # We avoid relying on depcrecated fixed-function pipeline functionality; any attempt to use legacy OpenGL calls
         # should fail.
         glFormat.setProfile(QtOpenGL.QGLFormat.CoreProfile)
@@ -40,64 +40,51 @@ class RisWidget(QtOpenGL.QGLWidget):
         glFormat.setDoubleBuffer(True)
         return glFormat
 
+    def _loadSource(self, sourceFileName):
+        with open(os.path.join(os.path.dirname(__file__), sourceFileName), 'r') as src:
+            return src.read()
+
+    def _getAttribLoc(self, attribName):
+        loc = GLS.glGetAttribLocation(self.shaderProgram, attribName)
+        if loc == -1:
+            raise RisWidgetException('Could not find location of panel.glslf attribute "{}".'.format(attribName))
+        return loc
+
     def initializeGL(self):
-        self.shaderProgram = QtGui.QOpenGLShaderProgram(self)
+        self.shaderProgram = GLS.compileProgram(
+            GLS.compileShader([self._loadSource('panel.glslv')], GLS.GL_VERTEX_SHADER),
+            GLS.compileShader([self._loadSource('image.glslf')], GLS.GL_FRAGMENT_SHADER))
+        GLS.glUseProgram(self.shaderProgram)
 
-        if not self.shaderProgram.addShaderFromSourceFile(QtGui.QOpenGLShader.Vertex, os.path.join(os.path.dirname(__file__), 'panel.glslv')):
-            raise ShaderCompilationException(self.shaderProgram.log())
+        self.panelVao = GL.glGenVertexArrays(1)
+        GL.glBindVertexArray(self.panelVao)
 
-        if not self.shaderProgram.addShaderFromSourceFile(QtGui.QOpenGLShader.Fragment, os.path.join(os.path.dirname(__file__), 'image.glslf')):
-            raise ShaderCompilationException(self.shaderProgram.log())
-
-        if not self.shaderProgram.link():
-            raise ShaderLinkingException(self.shaderProgram.log())
-
-        # Note: self.shaderProgram.bind() is equivalent to GL.glUseProgram(self.shaderProgram.programId())
-        if not self.shaderProgram.bind():
-            raise ShaderBindingException(self.shaderProgram.log())
-
-        self.panelVao = QtGui.QOpenGLVertexArrayObject(self)
-        if not self.panelVao.create():
-            raise RisWidgetException(self.shaderProgram.log())
-        self.panelVao.bind()
-
-        # Vertex positions
         quad = numpy.array([
-            0.75, -0.75,
-            -0.75, -0.75,
-            -0.75, 0.75,
-            0.75, 0.75,
+            # Vertex positions
+            0.98, -0.98,
+            -0.98, -0.98,
+            -0.98, 0.98,
+            0.98, 0.98,
             # Texture coordinates
             0.0, 0.0,
             1.0, 0.0,
             1.0, 1.0,
             0.0, 1.0], numpy.float32)
 
-        # Like other QtGui::QOpenGL.. primitives, QOpenGLBuffer's constructor does not assume that it is called
-        # from within a valid GL context and therefore does not complete all requisite setup.  NB:
-        # QtGui.QOpenGLBuffer.VertexBuffer represents GL_ARRAY_BUFFER.
-        self.quadShaderBuff = QtGui.QOpenGLBuffer(QtGui.QOpenGLBuffer.VertexBuffer)
-        if not self.quadShaderBuff.create():
-            raise BufferCreationException(self.shaderProgram.log())
-        self.quadShaderBuff.setUsagePattern(QtGui.QOpenGLBuffer.StaticDraw)
-        if not self.quadShaderBuff.bind():
-            raise BufferBindingException(self.shaderProgram.log())
-        self.quadShaderBuff.allocate(sip.voidptr(quad.data), quad.nbytes)
-        # quad is deleted to make clear that quad's data has been copied to the GPU by the .allocate call in the
-        # line above
+        self.quadShaderBuff = GL.glGenBuffers(1)
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.quadShaderBuff)
+        GL.glBufferData(GL.GL_ARRAY_BUFFER, quad, GL.GL_STATIC_DRAW)
+
+        # quad is deleted to make clear that quad's data has been copied to the GPU by the glBufferData directly above
         del quad
 
-        vertPosLoc = 0#self.shaderProgram.attributeLocation('vert_pos')
-        if vertPosLoc == -1:
-            raise RisWidgetException('Could not find location of panel.glslf attribute "vert_pos".')
-        self.shaderProgram.enableAttributeArray(vertPosLoc)
-        self.shaderProgram.setAttributeBuffer(vertPosLoc, GL.GL_FLOAT, 0, 2, 0)
+        vertPosLoc = self._getAttribLoc('vertPos')
+        GLS.glEnableVertexAttribArray(vertPosLoc)
+        GLS.glVertexAttribPointer(vertPosLoc, 2, GL.GL_FLOAT, False, 0, ctypes.c_void_p(0))
 
-        texCoordLoc = 1#self.shaderProgram.attributeLocation('tex_coord')
-        if texCoordLoc == -1:
-            raise RisWidgetException('Could not find location of panel.glslf attribute "tex_coord".')
-        self.shaderProgram.enableAttributeArray(texCoordLoc)
-        self.shaderProgram.setAttributeBuffer(texCoordLoc, GL.GL_FLOAT, 2 * 4 * 4, 2, 0)
+        texCoordLoc = self._getAttribLoc('texCoord')
+        GLS.glEnableVertexAttribArray(texCoordLoc)
+        GLS.glVertexAttribPointer(texCoordLoc, 2, GL.GL_FLOAT, False, 0, ctypes.c_void_p(2 * 4 * 4))
 
         checkerboard = numpy.array([
             [0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0x0000],
@@ -119,12 +106,11 @@ class RisWidget(QtOpenGL.QGLWidget):
         GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, GL.GL_CLAMP_TO_EDGE)
         GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_T, GL.GL_CLAMP_TO_EDGE)
 
-        pid = self.shaderProgram.programId()
-        self.gtpEnabledLoc = GL.glGetUniformLocation(pid, b'gtp.isEnabled')
-        self.gtpMinLoc     = GL.glGetUniformLocation(pid, b'gtp.minVal')
-        self.gtpMaxLoc     = GL.glGetUniformLocation(pid, b'gtp.maxVal')
-        self.gtpGammaLoc   = GL.glGetUniformLocation(pid, b'gtp.gammaVal')
-        self.projectionModelViewMatrixLoc = GL.glGetUniformLocation(pid, b'projectionModelViewMatrix')
+        self.gtpEnabledLoc = GLS.glGetUniformLocation(self.shaderProgram, b'gtp.isEnabled')
+        self.gtpMinLoc     = GLS.glGetUniformLocation(self.shaderProgram, b'gtp.minVal')
+        self.gtpMaxLoc     = GLS.glGetUniformLocation(self.shaderProgram, b'gtp.maxVal')
+        self.gtpGammaLoc   = GLS.glGetUniformLocation(self.shaderProgram, b'gtp.gammaVal')
+        self.projectionModelViewMatrixLoc = GLS.glGetUniformLocation(self.shaderProgram, b'projectionModelViewMatrix')
 
         self.qglClearColor(QtGui.QColor(255/3, 255/3, 255/3, 255))
 
@@ -135,9 +121,8 @@ class RisWidget(QtOpenGL.QGLWidget):
         GL.glClearDepth(1.0)
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
         if self.imTex is not None:
-            if not self.shaderProgram.bind():
-                raise ShaderBindingException(self.shaderProgram.log())
-            self.panelVao.bind()
+            GLS.glUseProgram(self.shaderProgram)
+            GL.glBindVertexArray(self.panelVao)
             GL.glBindTexture(GL.GL_TEXTURE_2D, self.imTex)
 
             # Rescale projection matrix such that display aspect ratio is preserved and contents fill either horizontal
@@ -151,14 +136,10 @@ class RisWidget(QtOpenGL.QGLWidget):
                     pmv[0, 0] = wsh/wsw
                 else:
                     pmv[1, 1] = wsw/wsh
-                GL.glUniformMatrix4fv(self.projectionModelViewMatrixLoc, 1, True, pmv)
+                GLS.glUniformMatrix4fv(self.projectionModelViewMatrixLoc, 1, True, pmv)
                 self.prevWindowSize = ws
 
-            try:
-                GL.glDrawArrays(GL.GL_TRIANGLE_FAN, 0, 4)
-            finally:
-                self.panelVao.release()
-                self.shaderProgram.release()
+            GL.glDrawArrays(GL.GL_TRIANGLE_FAN, 0, 4)
 
     def resizeGL(self, width, height):
         GL.glViewport(0, 0, width, height)
@@ -166,7 +147,7 @@ class RisWidget(QtOpenGL.QGLWidget):
     def showImage(self, imageData, recycleTexture = False):
         '''Enable rycleTexture when replacing an image that differs only by pixel value.  For example,
         do enable recycleTexture when showing frames from the same video file; do not enable recycleTexture
-        when displaying a random assortment of images with various sizes and color depths. '''
+        when displaying a random assortment of images of various sizes and color depths. '''
         if type(imageData) is not numpy.ndarray:
             raise TypeError('type(imageData) is not numpy.ndarray')
         if imageData.dtype != numpy.uint16:
@@ -175,7 +156,7 @@ class RisWidget(QtOpenGL.QGLWidget):
             raise ValueError('imageData.ndim != 2')
 
         self.context().makeCurrent()
-        self.shaderProgram.bind()
+        GLS.glUseProgram(self.shaderProgram)
 
         if not recycleTexture and self.imTex is not None:
             GL.glDeleteTextures([self.imTex])
@@ -202,66 +183,65 @@ class RisWidget(QtOpenGL.QGLWidget):
     def setGtpEnabled(self, gtpEnabled, update=True):
         '''Enable or disable gamma transformation.  If update is true, the widget will be refreshed.'''
         self.context().makeCurrent()
-        self.shaderProgram.bind()
-        GL.glUniform1i(self.gtpEnabledLoc, gtpEnabled)
+        GLS.glUseProgram(self.shaderProgram)
+        GLS.glUniform1i(self.gtpEnabledLoc, gtpEnabled)
         if update:
             self.update()
 
     def setGtpMinimum(self, gtpMinimum, update=True):
         '''Set gamma transformation minimum intensity parameter.  If update is true, the widget will be refreshed.'''
         self.context().makeCurrent()
-        self.shaderProgram.bind()
-        GL.glUniform1f(self.gtpMinLoc, gtpMinimum)
+        GLS.glUseProgram(self.shaderProgram)
+        GLS.glUniform1f(self.gtpMinLoc, gtpMinimum)
         if update:
             self.update()
 
     def setGtpMaximum(self, gtpMaximum, update=True):
         '''Set gamma transformation minimum intensity parameter.  If update is true, the widget will be refreshed.'''
         self.context().makeCurrent()
-        self.shaderProgram.bind()
-        GL.glUniform1f(self.gtpMaxLoc, gtpMaximum)
+        GLS.glUseProgram(self.shaderProgram)
+        GLS.glUniform1f(self.gtpMaxLoc, gtpMaximum)
         if update:
             self.update()
 
     def setGtpGamma(self, gtpGamma, update=True):
         '''Set gamma transformation gamma parameter.  If update is true, the widget will be refreshed.'''
         self.context().makeCurrent()
-        self.shaderProgram.bind()
-        GL.glUniform1f(self.gtpGammaLoc, gtpGamma)
+        GLS.glUseProgram(self.shaderProgram)
+        GLS.glUniform1f(self.gtpGammaLoc, gtpGamma)
         if update:
             self.update()
 
     def setGtpParams(self, gtpEnabled, gtpMinimum, gtpMaximum, gtpGamma, update=True):
         '''Set all gamma transformation parameters.  If update is true, the widget will be refreshed.'''
         self.context().makeCurrent()
-        self.shaderProgram.bind()
-        GL.glUniform1i(self.gtpEnabledLoc, gtpEnabled)
-        GL.glUniform1f(self.gtpMinLoc, gtpMinimum)
-        GL.glUniform1f(self.gtpMaxLoc, gtpMaximum)
-        GL.glUniform1f(self.gtpGammaLoc, gtpGamma)
+        GLS.glUseProgram(self.shaderProgram)
+        GLS.glUniform1i(self.gtpEnabledLoc, gtpEnabled)
+        GLS.glUniform1f(self.gtpMinLoc, gtpMinimum)
+        GLS.glUniform1f(self.gtpMaxLoc, gtpMaximum)
+        GLS.glUniform1f(self.gtpGammaLoc, gtpGamma)
         if update:
             self.update()
 
     def getGtpParams(self):
         '''Returns a dict containing all gamma transformation parameter values.'''
         self.context().makeCurrent()
-        pid = self.shaderProgram.programId()
         ret = {}
 
         v = numpy.array([-1], numpy.int32)
-        GL.glGetUniformiv(pid, self.gtpEnabledLoc, v)
+        GLS.glGetUniformiv(self.shaderProgram, self.gtpEnabledLoc, v)
         ret['gtpEnabled'] = True if v[0] == 1 else False
 
         v = numpy.array([None], numpy.float32)
-        GL.glGetUniformfv(pid, self.gtpMaxLoc, v)
+        GLS.glGetUniformfv(self.shaderProgram, self.gtpMaxLoc, v)
         ret['gtpMaximum'] = v[0]
 
         v[0] = numpy.nan
-        GL.glGetUniformfv(pid, self.gtpMinLoc, v)
+        GLS.glGetUniformfv(self.shaderProgram, self.gtpMinLoc, v)
         ret['gtpMinimum'] = v[0]
 
         v[0] = numpy.nan
-        GL.glGetUniformfv(pid, self.gtpGammaLoc, v)
+        GLS.glGetUniformfv(self.shaderProgram, self.gtpGammaLoc, v)
         ret['gtpGamma'] = v[0]
 
         return ret
