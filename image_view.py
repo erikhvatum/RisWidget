a7a04aeb5e283cf079f855b8c197b9bd608395ac
Erik Hvatum <ice.rikh@gmail.com>
2015-03-31 18:27:25 -0500

    Fix issue with incorrect mouseover info text positioning in ImageView when zoom level is changed when scrollbars are not visible.

--- image_view.py
+++ image_view.py
@@ -113,6 +113,7 @@ class ImageView(ShaderView):
                 self._ignore_zoom_to_fit_action_toggle = True
                 self.zoom_to_fit_action.setChecked(False)
                 self._ignore_zoom_to_fit_action_toggle = False
+            self.scene_view_rect_changed.emit()
             self.zoom_changed.emit(self._zoom_preset_idx, self._custom_zoom)
 
     def mousePressEvent(self, event):
@@ -173,3 +174,4 @@ class ImageView(ShaderView):
             self.translate(old_transform.dx(), old_transform.dy())
             self.scale(zoom_factor, zoom_factor)
             self.zoom_changed.emit(self._zoom_preset_idx, self._custom_zoom)
+        self.scene_view_rect_changed.emit()
