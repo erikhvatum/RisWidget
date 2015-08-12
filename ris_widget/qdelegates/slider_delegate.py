99e37a1c2bb7553b941253786ac40549b73d5902
Erik Hvatum <ice.rikh@gmail.com>
2015-08-11 23:05:54 -0500

    Fix minor opacity slider bugs.

--- ris_widget/qdelegates/slider_delegate.py
+++ ris_widget/qdelegates/slider_delegate.py
@@ -45,16 +45,24 @@ class SliderDelegate(Qt.QStyledItemDelegate):
         pbo.progress = int( (d-self.min_value)/(self.max_value-self.min_value) * 100.0 )
         pbo.text = '{}%'.format(pbo.progress)
         pbo.textVisible = True
+        pbo.textAlignment = Qt.Qt.AlignCenter
         pbo.rect = option.rect
         style = option.widget.style() if option.widget is not None else Qt.QApplication.style()
         style.drawControl(Qt.QStyle.CE_ProgressBar, pbo, qpainter)
 
     def editorEvent(self, event, model, option, midx):
+        print(option.rect, option.widget.visualRect(midx), option.widget.viewport().geometry())
         if not midx.isValid() or not event.type() == Qt.QEvent.MouseButtonPress or event.buttons() != Qt.Qt.LeftButton:
             return False
         if self._drag_grabber is not None:
             self._drag_grabber.deleteLater()
-        self._drag_grabber = DragGrabber(option.widget, model, option.rect, midx)
+        offset = option.widget.viewport().geometry().topLeft()
+        rect = Qt.QRect(
+            option.rect.left() + offset.x(),
+            option.rect.top() + offset.y(),
+            option.rect.size().width(),
+            option.rect.size().height())
+        self._drag_grabber = DragGrabber(option.widget, model, rect, midx)
         self._drag_grabber.destroyed.connect(self.on_drag_grabber_destroyed)
         self._drag_grabber.drag_x_changed.connect(self.on_drag_x_changed)
         return self.on_drag_x_changed(event.localPos().x(), option.rect, model, midx)
@@ -76,25 +84,49 @@ class DragGrabber(Qt.QWidget):
 
     def __init__(self, parent, model, rect, midx):
         super().__init__(parent)
+        self._ignore_next_no_button_mouse_move_event = True
         self.model = model
         self.midx = midx
         self.setMouseTracking(True)
+        # DragGrabber exactly covers the cell it manipulates, so it must be transparent (or simply
+        # not paint its background in the first place) in order for the slider to be visible
+        # while it is dragged.
         self.setAutoFillBackground(False)
-        self.setFocusPolicy(Qt.Qt.ClickFocus)
         self.setGeometry(rect)
         self.show()
-        self.setFocus(Qt.Qt.MouseFocusReason)
         self.grabMouse()
 
     def mouseReleaseEvent(self, event):
-        super().mouseReleaseEvent(event)
+        event.accept()
         self.releaseMouse()
         self.deleteLater()
 
     def mouseMoveEvent(self, event):
         event.accept()
         if event.buttons() != Qt.Qt.LeftButton:
-            self.releaseMouse()
-            self.deleteLater()
+            if event.buttons() == Qt.Qt.NoButton and self._ignore_next_no_button_mouse_move_event:
+                # In the definition of QApplicationPrivate::sendSyntheticEnterLeave(..), line 2788 of 
+                # qtbase-opensource-src-5.4.2/src/widgets/kernel/qapplication.cpp, is called when
+                # widget visibility changes, including as a result of the widget's first .show() call.
+                #
+                # The last two lines of sendSyntheticEnterLeave(..):
+                # QMouseEvent e(QEvent::MouseMove, pos, windowPos, globalPos, Qt::NoButton, Qt::NoButton, Qt::NoModifier);
+                # sendMouseEvent(widgetUnderCursor, &e, widgetUnderCursor, tlw, &qt_button_down, qt_last_mouse_receiver);
+                #
+                # So, even though sendSyntheticEnterLeave may be being called because a widget was shown
+                # in reponse to a mouse click, the first mouse event the newly created widget will receive
+                # if a number of conditions are met, and they are in this case, is the synthetic move event
+                # with flags set to indicate that it occurred with no mouse buttons depressed.  We consider
+                # dragging to have ended when the left mouse button is released, and so we must ignore this
+                # synethetic event if our DragGrabber is to exist for more than one event loop iteration.
+                self._ignore_next_no_button_mouse_move_event = False
+            else:
+                self.releaseMouse()
+                self.deleteLater()
         else:
             self.drag_x_changed.emit(event.x(), self.rect(), self.model, self.midx)
+
+    def focusOutEvent(self, event):
+        self.releaseMouse()
+        self.deleteLater()
+
