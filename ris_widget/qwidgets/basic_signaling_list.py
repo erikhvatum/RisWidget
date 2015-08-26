459d754c605ae7e9d5842d9f802df06154e16e7f
Erik Hvatum <ice.rikh@gmail.com>
2015-08-26 18:58:26 -0500

    Fix current row bug. Add an example demonstrating text eliding vs horizontal scrolling.

--- ris_widget/qwidgets/basic_signaling_list.py
+++ ris_widget/qwidgets/basic_signaling_list.py
@@ -50,14 +50,70 @@ class BasicSignalingListView(Qt.QListView):
 
     Usage example:
 
+    from PyQt5 import Qt
     from ris_widget.qwidgets.basic_signaling_list import BasicSignalingListView
-    bslv = BasicSignalingListView(list_=[1,2,3,4,'a'])
-    bslv.show()
-    bslv.signaling_list.extend(list('hello world'))
-    bslv.signaling_list = sorted(bslv.signaling_list, key=lambda e: str(e))
+    list_view = BasicSignalingListView(list_=[1,2,3,4,'a'])
+    list_view.setWindowTitle('Example: BasicSignalingListView + BasicSignalingListModel')
+    list_view.show()
+    list_view.signaling_list.extend(list('hello world'))
+    list_view.signaling_list = sorted(list_view.signaling_list, key=lambda e: str(e))
     def selection_changed(view, row):
         print('newly selected row:', row)
-    bslv.current_row_changed.connect(selection_changed)
+    list_view.current_row_changed.connect(selection_changed)
+
+    Usage example demonstrating horizontal resizing options:
+
+    from PyQt5 import Qt
+    from ris_widget.qwidgets.basic_signaling_list import BasicSignalingListView
+    class Example(Qt.QWidget):
+        def __init__(self, parent=None):
+            super().__init__(parent)
+            self.setWindowTitle('Example: BasicSignalingListView + BasicSignalingListModel + horizontal scrollbar vs eliding')
+            l = Qt.QVBoxLayout()
+            self.setLayout(l)
+            self.sort_button = Qt.QPushButton('lexicographically sort contents')
+            self.sort_button.clicked.connect(self.sort_list_contents)
+            l.addWidget(self.sort_button)
+            self.scroll_mode_button = Qt.QPushButton('scroll if too wide')
+            self.scroll_mode_button.clicked.connect(self.enter_scroll_mode)
+            l.addWidget(self.scroll_mode_button)
+            self.elide_mode_button = Qt.QPushButton('elide if too wide')
+            self.elide_mode_button.clicked.connect(self.enter_elide_mode)
+            l.addWidget(self.elide_mode_button)
+            self.current_row_num_label = Qt.QLabel()
+            l.addWidget(self.current_row_num_label)
+            self.list_view = BasicSignalingListView(list_=[1,2,3,4,'a'])
+            p = self.list_view.sizePolicy()
+            p.setHorizontalPolicy(Qt.QSizePolicy.Ignored)
+            self.list_view.setSizePolicy(p)
+            self.list_view.setTextElideMode(Qt.Qt.ElideNone)
+            self.signaling_list.append('a long string for provoking the need to scroll or elide')
+            self.signaling_list.extend(list('hello world'))
+            self.list_view.current_row_changed.connect(self.on_current_row_changed)
+            self.on_current_row_changed(self.list_view, self.list_view.selectionModel().currentIndex().row())
+            l.addWidget(self.list_view)
+        @property
+        def signaling_list(self):
+            return self.list_view.signaling_list
+        @signaling_list.setter
+        def signaling_list(self, v):
+            self.list_view.signaling_list = v
+        def sort_list_contents(self):
+            self.signaling_list = sorted(self.signaling_list, key=lambda e: str(e))
+        def enter_scroll_mode(self):
+            self.list_view.setTextElideMode(Qt.Qt.ElideNone)
+            self.list_view.setHorizontalScrollBarPolicy(Qt.Qt.ScrollBarAsNeeded)
+            self.list_view.scheduleDelayedItemsLayout()
+        def enter_elide_mode(self):
+            self.list_view.setTextElideMode(Qt.Qt.ElideMiddle)
+            self.list_view.setHorizontalScrollBarPolicy(Qt.Qt.ScrollBarAlwaysOff)
+            self.list_view.scheduleDelayedItemsLayout()
+        def on_current_row_changed(self, view, row):
+            self.list_view.setTextElideMode(Qt.Qt.ElideNone)
+            self.current_row_num_label.setText('row #: {}'.format(row))
+            self.list_view.scheduleDelayedItemsLayout()
+    example = Example()
+    example.show()
 
     Signals:
     * current_row_changed(BasicSignalingListView instance, row #): If no row is focused, row # is -1.'''
@@ -74,6 +130,8 @@ class BasicSignalingListView(Qt.QListView):
             model = BasicSignalingListModel(property_name, list_, parent)
         self.setModel(model)
         self.setDragDropMode(Qt.QAbstractItemView.DragDrop)
+        self.setDropIndicatorShown(True)
+        self.setDefaultDropAction(Qt.Qt.LinkAction)
         self.setSelectionMode(Qt.QAbstractItemView.SingleSelection)
         self.delete_current_row_action = Qt.QAction(self)
         self.delete_current_row_action.setText('Delete current row')
@@ -92,7 +150,7 @@ class BasicSignalingListView(Qt.QListView):
         if smidx.isValid():
             m.removeRow(smidx.row())
 
-    def _on_selection_model_current_row_changed(self, old_midx, new_midx):
+    def _on_selection_model_current_row_changed(self, new_midx, old_midx):
         self.current_row_changed.emit(self, new_midx.row())
 
     @property
