59e1c58ede96bcc4c7577c229f72a0a0ddcce190
Erik Hvatum <ice.rikh@gmail.com>
2015-09-19 19:35:20 -0500

    Tweak DropdownListDelegate positioning and highlight current value when shown.

--- ris_widget/qdelegates/dropdown_list_delegate.py
+++ ris_widget/qdelegates/dropdown_list_delegate.py
@@ -35,20 +35,23 @@ class DropdownListDelegate(Qt.QStyledItemDelegate):
         if not item_is_enabled or not item_is_editable:
             return False
         menu = Qt.QMenu(option.widget)
+        menu.setAttribute(Qt.Qt.WA_DeleteOnClose)
         choices = midx.data(CHOICES_QITEMDATA_ROLE)
         choice_actions = [menu.addAction(choice) for choice in choices]
         try:
             current_choice = midx.data()
             if isinstance(current_choice, Qt.QVariant):
                 current_choice = current_choice.value()
-            current_choice_idx = choices.index(current_choice)
+            current_choice_action = choice_actions[choices.index(current_choice)]
+            menu.setActiveAction(current_choice_action)
         except ValueError:
-            current_choice_idx = 0
-        menu.popup(option.widget.mapToGlobal(option.widget.visualRect(midx).topLeft()), choice_actions[current_choice_idx])
-        menu.setAttribute(Qt.Qt.WA_DeleteOnClose)
+            current_choice_action = None
+        cell_rect = option.widget.visualRect(midx)
+        menu_pos = option.widget.mapToGlobal(Qt.QPoint(cell_rect.left(), (cell_rect.top() + cell_rect.bottom())/2))
         pmidx = Qt.QPersistentModelIndex(midx)
         def on_entry_selected(action):
             if pmidx.isValid():
                 model.setData(model.index(pmidx.row(), pmidx.column()), action.text())
         menu.triggered.connect(on_entry_selected)
+        menu.popup(menu_pos, current_choice_action)
         return False
