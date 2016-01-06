5057621c7ca5d6bab03cfe77444fb8cb420ed502
Erik Hvatum <ice.rikh@gmail.com>
2016-01-05 19:37:56 -0600

    Add fix for 30-bit mode (may also fix OS X auto-hiding scrollbar flickering).

--- ris_widget/ris_widget.py
+++ ris_widget/ris_widget.py
@@ -82,6 +82,7 @@ class RisWidgetQtObject(Qt.QMainWindow):
         self.setAcceptDrops(True)
         self.layer_stack = LayerStack()
         self._init_scenes_and_views()
+        self._apply_deep_color_fix()
         self._init_flipbook()
         self._init_layer_stack_flipbook()
         self._init_actions()
@@ -91,6 +92,14 @@ class RisWidgetQtObject(Qt.QMainWindow):
             self.layer_stack.layers = layers
         import atexit
         atexit.register(_atexit)
+
+    def _apply_deep_color_fix(self):
+        """_apply_deep_color_fix(..) makes QOpenGLWidget contents visible in 30-bit mode.  This comes at the cost of preventing
+        other widgets from blending into QOpenGLWidgets, but has the additional benefit of eliminating an indirect drawing step,
+        making RisWidget a touch more responsive.  As distributed, RisWidget does not blend other widgets into QOpenGLWidget.
+        So, typically, no degradation apparent."""
+        self.main_view.gl_widget.setAttribute(Qt.Qt.WA_AlwaysStackOnTop)
+        self.histogram_view.gl_widget.setAttribute(Qt.Qt.WA_AlwaysStackOnTop)
 
     def _init_actions(self):
         self.layer_stack_reset_curr_min_max = Qt.QAction(self)
