cbe21593ed053a6de6d41c01086e7c6b037e63b4
Erik Hvatum <ice.rikh@gmail.com>
2016-04-19 21:12:52 -0500

    Add support for mask stretching to ndimage_statistics pure-Python fallback codepath.

--- ris_widget/ndimage_statistics/ndimage_statistics.py
+++ ris_widget/ndimage_statistics/ndimage_statistics.py
@@ -72,12 +72,23 @@ except ImportError:
     import warnings
     warnings.warn('warning: Failed to load _ndimage_statistics binary module; using slow histogram and extrema computation methods.')
 
+    def _stretch_mask(im, mask):
+        if im.shape != mask.shape:
+            x_lut = numpy.linspace(0, im.shape[0]-1, im.shape[0], dtype=numpy.uint32)
+            y_lut = numpy.linspace(0, im.shape[1]-1, im.shape[1], dtype=numpy.uint32)
+            xx_lut, yy_lut = numpy.meshgrid(x_lut, y_lut, indexing='ij')
+            mask = mask[xx_lut, yy_lut]
+        return mask
+
     def _min_max(im, mask=None):
         if mask is not None:
+            mask = _stretch_mask(im, mask)
             im = numpy.ma.array(im, dtype=im.dtype, copy=False, mask=~mask)
         return numpy.array((im.min(), im.max()), dtype=im.dtype)
 
     def _histogram(im, bin_count, range_, mask=None, with_overflow_bins=False):
+        if mask is not None:
+            mask = _stretch_mask(im, mask)
         if with_overflow_bins:
             assert bin_count >= 3
             hist = numpy.zeros((bin_count,), dtype=numpy.uint32)
@@ -102,7 +113,9 @@ except ImportError:
             range_ = (0,4095) if twelve_bit else (0,65535)
         else:
             raise NotImplementedError('Support for dtype of supplied im argument not implemented.')
-        hist = numpy.histogram(im, bins=bin_count, range=range, density=False, weights=mask)[0].astype(numpy.uint32)
+        if mask is not None:
+            mask = _stretch_mask(im, mask)
+        hist = numpy.histogram(im, bins=bin_count, range=range_, density=False, weights=mask)[0].astype(numpy.uint32)
         if mask is not None:
             im = numpy.ma.array(im, dtype=im.dtype, copy=False, mask=~mask)
         min_max[0] = im.min()
